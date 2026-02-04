e7d3efd6feb51056529eed03052ab5a0016b6c7e
VISHNU7KASIREDDY <vishnukasireddy28@gmail.com>
2026-02-04 21:55:00 +0530

    Update API URL

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const app=express();
 const connectDB=require('./config/db')
 
 app.use(cors({
-  origin: ['http://localhost:5174', 'http://localhost:5173'],
+  origin: '*',
   credentials: true
 }));
 app.use(express.json())
